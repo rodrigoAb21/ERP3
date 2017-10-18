7141ad37c65211d3b2c9142bfb96d2b8f502b95e
johanaZamora <jz89@hotmail.com>
2017-10-18 10:33:47 -0400

    gest.Permisos

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,5 +56,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'permisos'=>\App\Http\Middleware\CheckPermisos::class,
+
     ];
 }
