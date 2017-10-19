64c190aef63d0aacf7d59548a380cb327ac240c4
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2017-10-19 02:00:57 -0400

    u

--- resources/views/admin/Compras/producto/create.blade.php
+++ resources/views/admin/Compras/producto/create.blade.php
@@ -5,7 +5,7 @@
 
             <div class="col-md-9">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Crear Nuevo Producto</div>
+                    <div class="panel-heading">Crear Producto</div>
                     <div class="panel-body">
                         <a href="{{ url('/admin/producto') }}" title="Back"><button class="btn btn-warning btn-xs"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
                         <br />
