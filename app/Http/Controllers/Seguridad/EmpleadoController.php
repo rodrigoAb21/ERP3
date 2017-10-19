32068d2ee69a6b6e21f085cda402723f8d527595
johanaZamora <jz89@hotmail.com>
2017-10-18 22:15:15 -0400

    Prueba 3 Bit

--- app/Http/Controllers/Seguridad/EmpleadoController.php
+++ app/Http/Controllers/Seguridad/EmpleadoController.php
@@ -58,7 +58,9 @@ class EmpleadoController extends Controller
             {
                 $user2 -> idEmpleado = $empleado->id;
                 $user2->update();
-                Bitacora::registrarCreate(Utils::$TABLA_EMPLEADO,$empleado->id);
+                Bitacora::registrarCreate(
+                    Utils::$TABLA_EMPLEADO,$empleado->id
+                );
             }
 
         }
@@ -101,7 +103,9 @@ class EmpleadoController extends Controller
             $empleado -> tipo = 'Empleado';
             $empleado -> rol_id = $request->rol_id;
             $empleado -> update();
-            Bitacora::registrarUpdate(Utils::$TABLA_EMPLEADO,$empleado->id);
+            Bitacora::registrarUpdate(
+                Utils::$TABLA_EMPLEADO,$empleado->id
+            );
         }
         return Redirect::to('admin/empleados');
     }
