c801cea9bf53437c1298785d3492d30296bf06c4
johanaZamora <jz89@hotmail.com>
2017-10-30 21:38:36 -0400

    Update index.blade.php

--- resources/views/admin/Seguridad/rol/index.blade.php
+++ resources/views/admin/Seguridad/rol/index.blade.php
@@ -1,51 +1,51 @@
 @extends ('admin')
 @section ('contenido')
-                            <form method="post" class="form-inline well" action="{{url('/admin/rol/buscar')}}">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="depto">Busqueda por Rol:</label>
-                    <select name="depto" class="btn-lg btn-primary">
-                        @foreach($roles as $rol)
-                            <option value="{{$rol->id}}"
+    <form method="post" class="form-inline well" action="{{url('/admin/rol/buscar')}}">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="depto">Busqueda por Rol:</label>
+            <select name="depto" class="btn-lg btn-primary">
+                @foreach($roles as $rol)
+                    <option value="{{$rol->id}}"
                             @if($rolSearch->id==$rol->id) selected @endif
-                            >{{$rol->nombre}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                <button type="submit" class="btn btn-info">Buscar</button>
-                </div>
-            </form>
-
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Caso de uso</th>
-                    <th>Leer</th>
-                    <th>Crear</th>
-                    <th>Editar</th>
-                    <th>Eliminar</th>
-                    <th>Opcion</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                @foreach($rolSearch->casousos as $cu)
-                    <tr>
-                        <td>{{$cu->id}}</td>
-                        <td>{{$cu->nombre}}({{$cu->depto->nombre}})</td>
-                        <td><input type="checkbox" @if($cu->pivot->leer==1) checked @endif></td>
-                        <td><input type="checkbox" @if($cu->pivot->crear==1) checked @endif></td>
-                        <td><input type="checkbox" @if($cu->pivot->editar==1) checked @endif></td>
-                        <td><input type="checkbox" @if($cu->pivot->eliminar==1) checked @endif></td>
-                        <td><a href="{{url('/admin/acciones/'.$cu->id.'/'.$rolSearch->id)}}"
-                               class="btn btn-warning">Editar Acciones</a>
-                        </td>
-                    </tr>
+                    >{{$rol->nombre}}</option>
                 @endforeach
-                </tbody>
-            </table>
+            </select>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-info">Buscar</button>
+        </div>
+    </form>
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Caso de uso</th>
+            <th>Leer</th>
+            <th>Crear</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
+            <th>Opcion</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($rolSearch->casousos as $cu)
+            <tr>
+                <td>{{$cu->id}}</td>
+                <td>{{$cu->nombre}}({{$cu->depto->nombre}})</td>
+                <td><input type="checkbox" readonly="readonly" checked onclick="javascript: return false;"  @if($cu->pivot->leer==1) checked @endif></td>
+                <td><input type="checkbox" readonly="readonly" checked onclick="javascript: return false;" @if($cu->pivot->crear==1) checked @endif></td>
+                <td><input type="checkbox" readonly="readonly" checked onclick="javascript: return false;" @if($cu->pivot->editar==1) checked @endif></td>
+                <td><input type="checkbox" readonly="readonly" checked onclick="javascript: return false;" @if($cu->pivot->eliminar==1) checked @endif></td>
+                <td><a href="{{url('/admin/acciones/'.$cu->id.'/'.$rolSearch->id)}}"
+                       class="btn btn-warning">Editar Acciones</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 
 
 @endsection
