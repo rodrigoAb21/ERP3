0968b7b06f3ecdbb8169a6b275f264d99faa8a93
johanaZamora <jz89@hotmail.com>
2017-10-30 21:26:03 -0400

    Update index.blade.php

--- resources/views/admin/Seguridad/casouso/index.blade.php
+++ resources/views/admin/Seguridad/casouso/index.blade.php
@@ -1,16 +1,18 @@
 @extends ('admin')
 @section ('contenido')
-    <div class="col-sm-10">
+    <div class="col-sm-12">
 
 
-        <h2>Lista de Casos de Uso </h2>
+        <h3>Busqueda por Nombre de CU o por Departamento</h3>
 
         <form method="post" class="form-inline well" action="{{url('/admin/casouso/buscar')}}">
+
+            <br>
             {{ csrf_field() }}
             <div class="form-group">
                 <input type="text"
                        @if($casousoSearch=='')
-                       placeholder="empleado..."
+                       placeholder="nombre de cu..."
                        @else
                        value="{{$casousoSearch}}"
                        @endif
@@ -52,47 +54,5 @@
             @endforeach
             </tbody>
         </table>
-    </div>
-    <div class="col-sm-2">
-        <br><br>
-        <button class="btn btn-lg btn-primary" data-toggle="modal"
-                data-target="#crearCU">Crear CU
-        </button>
-    </div>
-
-
-    </div>
-
-    <!-- Modal crearCU -->
-    <div id="crearCU" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <form method="POST" action="{{url('/admin/casouso/guardar')}}">
-            {{ csrf_field() }}
-            <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Crear Nuevo Caso de Uso</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" name="nombre">
-                        </div>
-                        <div class="form-group">
-                            <select name="depto">
-                                <option value="0">Seleccione un Departamento</option>
-                                @foreach($deptos as $depto)
-                                    <option value="{{$depto->id}}">{{$depto->nombre}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-success">Guardar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
     </div>
 @endsection
