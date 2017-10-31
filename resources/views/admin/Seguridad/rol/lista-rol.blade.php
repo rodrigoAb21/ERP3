b08c9afab7c85dc5dbcebf46eda546ad467dec26
johanaZamora <jz89@hotmail.com>
2017-10-30 21:16:51 -0400

    Update lista-rol.blade.php

--- resources/views/admin/Seguridad/rol/lista-rol.blade.php
+++ resources/views/admin/Seguridad/rol/lista-rol.blade.php
@@ -1,9 +1,9 @@
 @extends ('admin')
 @section ('contenido')
     <div class="col-sm-10">
+        
             <h3>Roles de Usuario del Sistema</h3>
-    
-           <table class="table table-hover">
+         <table class="table table-hover">
                 <thead>
                 <tr>
                     <th>Nombre del Rol</th>
@@ -20,33 +20,7 @@
                 @endforeach
                 </tbody>
             </table>
-        {{ $roles->links() }}
         </div>
     </div>
-
-
-    <!-- Modal crear Rol -->
-    <div id="crearRol" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <form method="POST" action="{{url('/admin/rol/guardar')}}">
-            {{ csrf_field() }}
-            <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Nuevo Rol</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" name="nombre">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-success">Guardar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+    
 @endsection
