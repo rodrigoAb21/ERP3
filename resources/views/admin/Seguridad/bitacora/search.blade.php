3592c88daf7e3c910da311b1cbdf0ff55ebe0e3d
johanaZamora <jz89@hotmail.com>
2017-10-30 21:11:54 -0400

    Update search.blade.php

--- resources/views/admin/Seguridad/bitacora/search.blade.php
+++ resources/views/admin/Seguridad/bitacora/search.blade.php
@@ -3,10 +3,10 @@
 	<div class="form-group">
 		<label for="tiempo">Desde :</label><br>
 		<select name="tiempo" class="btn btn-default form-control">
-			<option value="1" @if ($tiempo==1) selected @endif >Hoy</option>
-			<option value="2" @if ($tiempo==2) selected @endif >Ayer</option>
-			<option value="3" @if ($tiempo==3) selected @endif>Hace 1 Semana</option>
-			<option value="4" @if ($tiempo==4) selected @endif>Hace 1 mes</option>
+			<option value="1" >Hoy</option>
+			<option value="2"  >Ayer</option>
+			<option value="3" >Hace 1 Semana</option>
+			<option value="4" >Hace 1 mes</option>
 		</select>
 	</div>
 	<div class="form-group">
