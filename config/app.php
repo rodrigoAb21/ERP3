96d8813e4326656e1f9357e620d85bf8c8dcb37c
johanaZamora <jz89@hotmail.com>
2017-10-30 20:45:10 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -179,7 +179,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Anchu\Ftp\FtpServiceProvider::class,
-        Laracasts\Flash\FlashServiceProvider::class,
+        //Laracasts\Flash\FlashServiceProvider::class,
 
     ],
 
