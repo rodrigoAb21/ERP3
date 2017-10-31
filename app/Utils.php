900e0589b5111ee7371ccbdacf28002180925a9a
johanaZamora <jz89@hotmail.com>
2017-10-30 22:25:39 -0400

    Update Utils.php

--- app/Utils.php
+++ app/Utils.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: johana
- * Date: 30-09-17
- * Time: 02:10 PM
- */
-
 namespace App;
 
 
 class Utils
 {
-
+    public static $ACTION_LISTAR='VER';
     public static $ACTION_CREATE='CREAR';
     public static $ACTION_UPDATE='ACTUALIZAR';
     public static $ACTION_DELETE='ELIMINAR';
@@ -24,13 +17,6 @@ class Utils
     public static $TABLA_CLIENTE='cliente';
     public static $TABLA_SEGUIMIENTO='seguimiento';
     public static $TABLA_TAREA='tarea';
-    public static $TABLA_ESTADO='estado';
-
-    public static $TABLA_PROVEEDOR='proveedor';
-    public static $TABLA_CATEGORIA_PRODUCTO='categoria_producto';
-    public static $TABLA_TIPO='tipo';
-    public static $TABLA_PRODUCTO='producto';
-
 
 
 }
