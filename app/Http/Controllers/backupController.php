de840744baabebf09325e065d86fd87de2828684
JhordanF <JhordanF1@hotmail.com>
2017-11-05 15:11:09 -0400

    backuprestore2

--- app/Http/Controllers/backupController.php
+++ app/Http/Controllers/backupController.php
@@ -145,8 +145,8 @@ class backupController extends Controller
 
         //           mysql -u $db_user -p $db_pass -e "source /ruta/a/respaldo.sql"
         //mysql -uroot -psa -h 127.0.0.1 Aperco < c:\backup\Bckarchio.sql
-        set_time_limit(200);
-        $dump = "mysql -h$db_host -u$db_user -p$db_pass $db_name < $datosA";
+       // set_time_limit(200);  //-h$db_host
+        $dump = "mysql -u$db_user -p$db_pass $db_name < $datosA";
         system($dump, $output);
 
 
