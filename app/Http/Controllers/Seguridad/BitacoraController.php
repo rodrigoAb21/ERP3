836600613fdb124c73a2b69b2b8c0adcd21efe73
johanaZamora <jz89@hotmail.com>
2017-11-06 10:03:04 -0400

    Add files via upload

---
+++ app/Http/Controllers/Seguridad/BitacoraController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Seguridad;
+
+use App\Modelos\Seguridad\Bitacora;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class BitacoraController extends Controller
+{
+
+
+    public function index()
+    {
+        $bitacoras = Bitacora::where('empresa_id','=',Auth::user()->idEmpresa)
+                    ->orderBy('fechaEntrada','ASC')
+                    ->get();
+        return view('admin.Seguridad.bitacora.index',
+            compact('bitacoras','empleados','tiempo','user_id'));
+    }
+    public function show($id)
+    {
+        $bitacora=Bitacora::findOrFail($id);
+        return view('admin.Seguridad.bitacora.show',compact('bitacora'));
+    }
+
+    public function getFecha($tiempo)
+    {
+        $desde='';
+        switch ($tiempo) {
+            case 1://hoy
+                $desde = '2017-10-18';
+                break;
+            case 2://ayer
+                $desde = '2017-10-17';
+                break;
+            case 3://1semana
+                $desde = '2017-10-11';
+                break;
+            case 4://1 mes
+                $desde = '2017-9-28';
+                break;
+        }
+        return $desde;
+    }
+}
