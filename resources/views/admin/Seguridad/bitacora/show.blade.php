6c9a19d00804970c7ef1625409dea946b67ca832
johanaZamora <jz89@hotmail.com>
2017-11-06 10:05:16 -0400

    Update show.blade.php

--- resources/views/admin/Seguridad/bitacora/show.blade.php
+++ resources/views/admin/Seguridad/bitacora/show.blade.php
@@ -3,29 +3,35 @@
 
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	<h4>Acciones realizadas por el Usuario<b> {{$bitacora->user->name}}</b></h4><br>
+	<a href="{{url('/admin/bitacora')}}" class="btn btn-info btn-sm">Atras</a>
+	<h4>Acciones realizadas por el Usuario<b> {{ $bitacora->user->name.' ( '.$bitacora->user->empleado->rol->nombre.' )'}}</b></h4><br>
 	</div>
 </div>
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<div class="table-responsive">
-			<table class="table table-striped table-condensed table-hover table-bordered">
-				<thead>
-                    <th>#</th>
-                    <th>Accion Realizada</th>
-                    <th>Tabla Afectada</th>
-					<th>Numero de Tupla</th>
-				</thead>
-               @foreach ($bitacora->acciones as $accione)
-				<tr>
-                    <td>{{ $accione->id}}</td>
-                    <td>{{ $accione->accion}}</td>
-					<td>{{$accione->tabla}}</td>
-					<td>{{$accione->tupla}}</td>
+			@if($bitacora->acciones=='[]')
+				<h3>El usuario no ha realizado acciones.</h3>
+			@else
+				<table class="table table-striped table-condensed table-hover table-bordered">
+					<thead>
+					<th>#</th>
+					<th>Accion Realizada</th>
+					<th>Tabla Afectada</th>
+					<th>Tupla Afectada</th>
+					</thead>
+					@foreach ($bitacora->acciones as $accione)
+						<tr>
+							<td>{{ $accione->id}}</td>
+							<td>{{ $accione->accion}}</td>
+							<td>{{$accione->tabla}}</td>
+							<td>{{$accione->tupla}}</td>
 
-				</tr>
-				@endforeach
-			</table>
+						</tr>
+					@endforeach
+				</table>
+			@endif
+
 		</div>
 	</div>
 </div>
