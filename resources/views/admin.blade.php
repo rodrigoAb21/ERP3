3bb4a3ecdd2fc612072508f0466026aa239ac4e1
johanaZamora <jz89@hotmail.com>
2017-11-07 06:06:51 -0400

    Update admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -188,7 +188,8 @@
                           <li><a href="{{asset('/admin/beneficios')}}"><i class="fa fa-arrow-right"></i> Gestionar Beneficios</a></li>
                           <li><a href="{{asset('/admin/tareas')}}"><i class="fa fa-arrow-right"></i> Gestionar Tareas</a></li>
                           <li><a href="{{asset('/admin/seguimientos')}}"><i class="fa fa-arrow-right"></i> Gestionar Seguimientos</a></li>
-                          <li><a href="{{asset('/admin/categoria')}}"><i class="fa fa-arrow-right"></i> Gestionar Categoria</a></li>
+                           <li><a href="{{url('admin/categoria')}}"><i class="fa fa-arrow-right"></i> Gestionar Categoria</a></li>
+                          <li><a href="{{url('admin/promocion')}}"><i class="fa fa-arrow-right"></i> Gestionar Promocion</a></li>
                       </ul>
                   </li>
                   <li class="treeview">
